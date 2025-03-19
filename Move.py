c26877a34a4b7ef99a58fbb85a6aeaacaa2e6b23
jakubkalinski0 <kubakalinski2@gmail.com>
2025-03-19 20:07:11 +0100

    Create Move.py

---
+++ Move.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+from MoveType import MoveType
+from Position import Position
+
+class Move:
+    """Representation of movement in the game."""
+    def __init__(self, move_type: MoveType, from_position: Optional[Position] = None, to_position: Optional[Position] = None, remove_position: Optional[Position] = None):
+        self.move_type = move_type
+        self.from_position = from_position
+        self.to_position = to_position
+        self.remove_position = remove_position
+
+    def __eq__(self, other):
+        if not isinstance(other, Move):
+            return False
+        return (self.move_type == other.move_type and
+                self.from_position == other.from_position and
+                self.to_position == other.to_position and
+                self.remove_position == other.remove_position)
+
+    def __hash__(self):
+        return hash((self.move_type, self.from_position, self.to_position, self.remove_position))
