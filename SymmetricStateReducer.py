eef1195c71d9c8f20d8cb0b919c6893f079465fb
jakubkalinski0 <kubakalinski2@gmail.com>
2025-04-02 18:15:41 +0200

    Created SymmetricStateReducer.py class for future development

---
+++ SymmetricStateReducer.py
@@ -0,0 +1,14 @@
+import numpy as np
+from BoardState import BoardState
+from Player import Player
+
+
+class SymmetricStateReducer:
+    """
+    Utility class to reduce the state space of Morris games by identifying symmetric positions.
+    This helps with memory reduction for state storage and learning algorithms.
+    """
+
+    def __init__(self, board_type):
+        """Initialize with the board type and set up the symmetry matrices."""
+        self
