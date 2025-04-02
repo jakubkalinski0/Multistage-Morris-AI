e7f811c926d18779717dcc1c2545ece5193a1153
jakubkalinski0 <kubakalinski2@gmail.com>
2025-04-02 06:07:14 +0200

    Added switch_player and improved opponent methods in Player.py

--- Player.py
+++ Player.py
@@ -1,14 +1,22 @@
 from enum import Enum
 
+
 class Player(Enum):
     """Player representation."""
     NONE = 0
     WHITE = 1
     BLACK = 2
-    def opponent(self, Player):
+
+    def opponent(self):
         """Returns the opponent of the player."""
         if self == Player.WHITE:
             return Player.BLACK
         elif self == Player.BLACK:
             return Player.WHITE
         return Player.NONE
+
+    def switch_player(self, players):
+        """Switches between two players."""
+        if self == players[0]:
+            return players[1]
+        return players[0]
