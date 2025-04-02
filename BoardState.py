3e5fa5bbc177a6c99c9f9650e0559c2cbf56b651
Kacperon <kacper.feliks.455@gmail.com>
2025-04-02 16:09:46 +0200

    Add integer representation methods to BoardState

--- BoardState.py
+++ BoardState.py
@@ -8,12 +8,11 @@ from Position import Position
 class BoardState:
     def __init__(self, board_size: int, pieces_per_player: int):
         self.board_size = board_size
+        self.who_wins = 0
         self.board = [Player.NONE]*board_size
         self.pieces_per_player = pieces_per_player
-        self.game_phase_for_player = {
-            Player.WHITE: GamePhase.PLACEMENT,
-            Player.BLACK: GamePhase.PLACEMENT
-        }
+        self.game_phase = GamePhase.PLACEMENT # white starts first placment change when black ends phase
+
         self.pieces_left_to_place_by_player = {
             Player.WHITE: pieces_per_player,
             Player.BLACK: pieces_per_player
@@ -40,3 +39,24 @@ class BoardState:
 
     def __str__(self):
         return NotImplemented
+
+    def to_int(self):
+        """Converts the board state to an integer representation."""
+        result = self.who_wins
+        result+= 3 * self.curent_player.value
+        for i in range(self.board_size):
+            result+= self.board[i].value * (3 ** (i+2) )
+        return result
+
+    def from_int(self, int_representation: int):
+        """Converts an integer representation to a board state."""
+        self.who_wins = int_representation % 3
+        int_representation //= 3
+        self.current_player = Player(int_representation % 3)
+        int_representation //= 3
+        for i in range(self.board_size):
+            self.board[i] = Player(int_representation % 3)
+            int_representation //= 3
+        
+
+    
